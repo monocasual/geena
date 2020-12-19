1f92ec4251b466118bce2a703cc7df7dfa1fe18e
gvnnz <monocasual@gmail.com>
2020-12-19 21:30:29 +0100

    DRY

--- src/ui/mainWindow.cpp
+++ src/ui/mainWindow.cpp
@@ -15,13 +15,62 @@ namespace geena::ui
 namespace
 {
 bool keyPressed_ = false;
+
+
+/* -------------------------------------------------------------------------- */
+
+
+void onKeyDown_(int key)
+{
+	if (key == FL_Up)
+		engine::api::setPitch(engine::PitchDir::UP);
+	else
+	if (key == FL_Down)
+		engine::api::setPitch(engine::PitchDir::DOWN);
+	else
+	if (key == FL_Left)
+		engine::api::nudgePitch_begin(engine::PitchDir::DOWN);
+	else
+	if (key == FL_Right)
+		engine::api::nudgePitch_begin(engine::PitchDir::UP);
 }
+
+
+/* -------------------------------------------------------------------------- */
+
+
+void onKeyUp_(int key)
+{
+	if (key == FL_Left || key == FL_Right)
+		engine::api::nudgePitch_end();
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+
+void onFileDrop_(const char* s)
+{
+	std::vector<std::string> paths = utils::string::split(s, "\n");
+	std::string path = utils::fs::uriToPath(paths[0]);
+	engine::api::loadAudioFile(path);	
+}
+} // {anonymous}
+
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
 
 MainWindow::MainWindow()
 : Fl_Window(640, 480)
 {
 	show();
 }
+
+
+/* -------------------------------------------------------------------------- */
 
 
 int MainWindow::handle(int event)
@@ -31,30 +80,19 @@ int MainWindow::handle(int event)
 		case FL_FOCUS:
 		case FL_UNFOCUS: 
 		{
-			return 1;   // Enables receiving Keyboard events
+			return 1;   // Enables receiving keyboard events
 		}
 		case FL_KEYDOWN:
 		{
 			if (keyPressed_)
 				break;
-			if (Fl::event_key() == FL_Up)
-				engine::api::setPitch(engine::PitchDir::UP);
-			else
-			if (Fl::event_key() == FL_Down)
-				engine::api::setPitch(engine::PitchDir::DOWN);
-			else
-			if (Fl::event_key() == FL_Left)
-				engine::api::nudgePitch_begin(engine::PitchDir::DOWN);
-			else
-			if (Fl::event_key() == FL_Right)
-				engine::api::nudgePitch_begin(engine::PitchDir::UP);
+			onKeyDown_(Fl::event_key());
 			keyPressed_ = true;
 			return 1;
 		}
 		case FL_KEYUP:
 		{
-			if (Fl::event_key() == FL_Left || Fl::event_key() == FL_Right)
-				engine::api::nudgePitch_end();
+			onKeyUp_(Fl::event_key());
 			keyPressed_ = false;
 			return 1;
 		}
@@ -66,9 +104,7 @@ int MainWindow::handle(int event)
 		}
 		case FL_PASTE:  // drop (paste) operation
 		{
-			std::vector<std::string> paths = utils::string::split(Fl::event_text(), "\n");
-			std::string path = utils::fs::uriToPath(paths[0]);
-			engine::api::loadAudioFile(path);
+			onFileDrop_(Fl::event_text());
 			return 1;
 		}
 		default:
@@ -77,6 +113,9 @@ int MainWindow::handle(int event)
 
 	return Fl_Window::handle(event);
 }
+
+
+/* -------------------------------------------------------------------------- */
 
 
 int MainWindow::run()
