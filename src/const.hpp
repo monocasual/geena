e62d05424ce133b193a070befc3ccf7e9f6e52f0
gvnnz <monocasual@gmail.com>
2021-01-06 22:35:34 +0100

    Finer pitch adjustments

--- src/const.hpp
+++ src/const.hpp
@@ -5,6 +5,6 @@ namespace geena
 {
 constexpr float G_UI_REFRESH_RATE = 1 / 24.0f;  // 24 times per second
 
-constexpr float G_PITCH_DELTA = 0.005;
-constexpr float G_PITCH_NUDGE = 0.1;
+constexpr float G_PITCH_DELTA = 0.0025;
+constexpr float G_PITCH_NUDGE = 0.05;
 } // geena
