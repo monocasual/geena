6ef8790e0723cc13ffd89e0ff196be66df8faa3a
gvnnz <monocasual@gmail.com>
2022-04-23 18:03:02 +0200

    Smaller pitch delta

--- src/const.hpp
+++ src/const.hpp
@@ -8,6 +8,6 @@ constexpr float G_DEFAULT_PITCH = 1.0;  //   0%
 constexpr float G_MIN_PITCH     = 0.84; // -16%
 constexpr float G_MAX_PITCH     = 1.16; // +16%
 
-constexpr float G_PITCH_DELTA = 0.0025;
+constexpr float G_PITCH_DELTA = 0.00125;
 constexpr float G_PITCH_NUDGE = 0.05;
 } // namespace geena
