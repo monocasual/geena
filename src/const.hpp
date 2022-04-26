4bfb8c94b4fb03eedc8103ce762864b08e6b9e25
gvnnz <monocasual@gmail.com>
2022-04-26 21:52:50 +0200

    +-10 pitch

--- src/const.hpp
+++ src/const.hpp
@@ -5,9 +5,9 @@ namespace geena
 constexpr float G_UI_REFRESH_RATE = 1 / 24.0f; // 24 times per second
 
 constexpr float G_DEFAULT_PITCH = 1.0;  //   0%
-constexpr float G_MIN_PITCH     = 0.84; // -16%
-constexpr float G_MAX_PITCH     = 1.16; // +16%
+constexpr float G_MIN_PITCH     = 0.90; // -10%
+constexpr float G_MAX_PITCH     = 1.10; // +10%
 
-constexpr float G_PITCH_DELTA = 0.00125;
+constexpr float G_PITCH_DELTA = 0.000625;
 constexpr float G_PITCH_NUDGE = 0.05;
 } // namespace geena
