48e202e21a25d87aa100d9a601d8e3cfd6a17e49
gvnnz <monocasual@gmail.com>
2022-05-09 18:46:32 +0200

    Increase seek length

--- src/const.hpp
+++ src/const.hpp
@@ -14,5 +14,5 @@ constexpr float G_PITCH_DELTA = 0.000625;
 constexpr float G_PITCH_NUDGE = 0.05;
 
 constexpr Frame G_SEEK_STEP   = 256;
-constexpr Frame G_SEEK_LENGTH = 4096;
+constexpr Frame G_SEEK_LENGTH = 8192;
 } // namespace geena
